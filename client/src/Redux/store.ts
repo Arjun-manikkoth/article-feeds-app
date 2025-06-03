a423444e9d0c26b48ae0d209ad2225229cd362bd
Arjun-Manikkoth <arjunmanikkoth28@gmail.com>
2025-06-03 16:15:20 +0530

    fix: fix userslice module path import error at redux store

--- client/src/Redux/store.ts
+++ client/src/Redux/store.ts
@@ -1,7 +1,7 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
-import userReducer from "./Slices/userSlice";
+import userReducer from "./Slices/UserSlice";
 
 const persistConfig = {
     key: "root",
